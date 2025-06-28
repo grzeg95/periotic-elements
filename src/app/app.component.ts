6b9a39a2b3b16cb684b13a988f1f42caf02a73e9
Grzegorz Eleryk <dev.grzegorz.eleryk@gmail.com>
2025-06-28 12:36:04 +0200

    feat: add displaying of periodic elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {MatProgressBar} from '@angular/material/progress-bar';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow,
+  MatHeaderRowDef,
+  MatRow,
+  MatRowDef,
+  MatTable
+} from '@angular/material/table';
 import {PeriodicElementsStore} from './store/periodic-elements-store';
 
 @Component({
   selector: 'app-root',
   imports: [
-    MatProgressBar
+    MatProgressBar,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatHeaderCellDef,
+    MatCellDef,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
@@ -13,6 +35,8 @@ import {PeriodicElementsStore} from './store/periodic-elements-store';
 export class AppComponent implements OnInit {
 
   protected readonly _periodicElementsStore = inject(PeriodicElementsStore);
+
+  protected _displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
 
   ngOnInit(): void {
     this._periodicElementsStore.loadAll();
